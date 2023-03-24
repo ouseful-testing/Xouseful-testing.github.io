4104ee4e0d4dc37129a359f3bbc5a37c37a23e1e
Tony Hirst <tony.hirst@gmail.com>
2023-03-24 11:25:32 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,17 @@
 ---
 
 {% include js/conference.js %}
+
+window.conference.awaitReady().then(() => {
+    const map = window.conference.map.getMap();
+
+    if (typeof map !== 'undefined') {
+        let iwsr_station = L.marker([50.7051893, -1.2167519], {
+            icon: L.divIcon({
+                className: '',
+                html: '<span class="fas fa-train"></span> Isle of Wight Steam Railway',
+                iconSize: [120, 56]
+            })
+        }).addTo(map);
+    }
+});
