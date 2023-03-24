2591bdf01c3572138e41c08289266d6c11fd03cd
Tony Hirst <tony.hirst@gmail.com>
2023-03-24 11:30:48 +0000

    simple js map marker

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,6 @@ window.conference.awaitReady().then(() => {
     const map = window.conference.map.getMap();
 
     if (typeof map !== 'undefined') {
-        let iwsr_station = L.marker([50.7051893, -1.2167519], {
-            icon: L.divIcon({
-                className: '',
-                html: '<span class="fas fa-train"></span> Isle of Wight Steam Railway',
-                iconSize: [120, 56]
-            })
-        }).addTo(map);
+        let iwsr_station = L.marker([50.7051893, -1.2167519]).addTo(map);
     }
 });
